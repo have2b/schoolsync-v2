7f811361281d09b0395af19dcfa34a14c9b74659
tlongizunique <thanhlongvu156@gmail.com>
2024-11-22 06:31:28 +0700

    fix: not found in db name

--- src/db/reset-db.ts
+++ src/db/reset-db.ts
@@ -13,28 +13,35 @@ const resetDatabase = async () => {
   })
 
   try {
+    console.log('---- Resetting database...')
+    console.log('---- Connecting to database...')
     // Connect to management database
     await managementClient.connect()
 
+    console.log('---- Killing all database connections...')
     // Kill all connections to database
     await managementClient.query(`
-      SELECT pg_terminate_backend(pid) 
-      FROM pg_stat_activity 
-      WHERE datname = ${DB_NAME};
+      SELECT pg_terminate_backend(pid)
+      FROM pg_stat_activity
+      WHERE datname = '${DB_NAME}';
     `)
 
+    console.log('---- Dropping and creating database...')
     // Drop and create database
     await managementClient.query(`DROP DATABASE IF EXISTS ${DB_NAME};`)
     await managementClient.query(`CREATE DATABASE ${DB_NAME};`)
 
+    console.log('---- Closing connection...')
     // Close management connection
     await managementClient.end()
+
+    console.log('---- Reset database success!')
   } catch (error) {
     console.error('Database reset error:', error)
     throw error
   }
 }
 
-resetDatabase()
+await resetDatabase()
   .then(() => process.exit())
   .catch(console.error)
