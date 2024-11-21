0a2b1f0dca6aba683c2f9b65204d9cfe52049ee4
tlongizunique <thanhlongvu156@gmail.com>
2024-11-21 09:31:19 +0700

    feat: define enums for schema and connection for db

---
+++ src/db/schema.ts
@@ -0,0 +1,3 @@
+export const Role = ['Sinh Vien', 'Giang Vien', 'Quan Tri'] as const
+export const Gender = ['Nam', 'Nu', 'Khac'] as const
+export const Degree = ['Cu Nhan', 'Thac Si', 'Tien Si', 'Pho Giao Su', 'Giao Su'] as const
